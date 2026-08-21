ef0822c2778705873e20456ac0f2a8980d7e2033
Amarjeet singh <amarjeetkumarsingh0009@gmail.com>
2026-08-21 17:22:45 +0530

    Add initial 2026-08-19 Python program for variables and operators
    This program demonstrates variables and operators in Python, including swapping values and performing arithmetic operations.

---
+++ INT-108/int108.py
@@ -0,0 +1,26 @@
+'''This is the first program 
+of python to explain
+variables and operators'''
+print("hello world")
+
+_A=1
+a1=2
+
+print(_A+a1)
+
+a = 8
+b = 5
+print("before swapping:", a, b)
+a = a + b
+b = a - b
+a = a - b
+print("after swapping:", a, b)
+
+
+print("a+b =" ,a+b)
+print("a-b =" ,a-b)
+print("a/b =" ,a/b)
+print("a//b =" ,a//b)
+print("a**b =" ,a**b)
+print("a*b =" ,a*b)
+print("a%b =" ,a%b)
