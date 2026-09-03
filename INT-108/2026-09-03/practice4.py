efdfbb680a1a176155bdd53186c8cabe91e7c86e
Amarjeet singh <amarjeetkumarsingh0009@gmail.com>
2026-09-03 20:53:14 +0530

    replace the second error code by the help of adarsh that is extra and unnesseciary use of if else

--- INT-108/2026-09-03/practice4.py
+++ INT-108/2026-09-03/practice4.py
@@ -1,12 +1,9 @@
 a = int(input("Enter first side length: "))
 b = int(input("Enter second side length: "))
 c = int(input("Enter third side length: "))
-if(type(a)==int and type(b)==int and type(c)==int):
-    if (a==b and b==c):
-        print("equilateral")
-    elif (a!=b and b!=c and c!=a):
-        print("scalene")
-    elif (a==b or b==c or c==a):
-        print("isosceles")
-else:
-    print("please enter length in integer")
+if (a==b and b==c):
+    print("equilateral")
+elif (a!=b and b!=c and c!=a):
+    print("scalene")
+elif (a==b or b==c or c==a):
+    print("isosceles")
